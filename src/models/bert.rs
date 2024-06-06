6f6810b36655fb53d5ea847532b1b6b2f15a4aef
Scott <55364794+scottroot@users.noreply.github.com>
2024-06-06 04:15:53 -0400

    Update bert.rs

--- src/models/bert.rs
+++ src/models/bert.rs
@@ -65,10 +65,9 @@ impl Args {
         Ok((model, tokenizer))
     }
     fn _encode_text(&self) -> LuaResult<Vec<f32>> {
-        eprintln!("Starting _encode_text");
         // let (model, mut tokenizer) = self.build_model_and_tokenizer()
         //     .map_err(|err| LuaError::external(err))?;
-        ////////////////////////////////////////////////////////////////////////////////////////////
+        //
         let config = b64.decode(&self.config)
             .map_err(|err| {
                 eprintln!("!! Error during b64 decode config\n{}", err);
@@ -79,33 +78,25 @@ impl Args {
                 eprintln!("!! Error during serde_json::from_value\n{}", err);
                 LuaError::external(err)
             })?;
-        eprintln!("Built mut config");
         if self.approximate_gelu {
             config.hidden_act = HiddenAct::GeluApproximate;
         }
-        eprintln!("Maybe added approx gelu");
 
-        // let model_bytes: Vec<u8> = self.model.clone();
-        eprintln!("Let model_bytes");
-        let vb = VarBuilder::from_buffered_safetensors(self.model.clone(), DTYPE,  &self.device)
+        let model_bytes: Vec<u8> = self.model.clone();
+        let vb = VarBuilder::from_buffered_safetensors(model_bytes, DTYPE,  &self.device)
             .map_err(|err| LuaError::external(err))?;
-        eprintln!("Got VB");
         let model = BertModel::load(vb, &config)
             .map_err(|err| {
-                eprintln!("!! Error model = BertModel:: ...\n{}", err);
+                eprintln!("!! Error on BertModel::load()\n{}", err);
                 LuaError::external(err)
             })?;
-        eprintln!("Did let model = BertModel::load(vb, &config)");
 
         let mut tokenizer = Tokenizer::from_bytes(self.tokenizer.clone())
             .map_err(|err| {
-                eprintln!("!! Error during Tokenizer::from_bytes\n{}", err);
+                eprintln!("!! Error on Tokenizer::from_bytes\n{}", err);
                 LuaError::external(err)
             })?;
-        eprintln!("Did let mut tokenizer = Tokenizer::from_bytes(s");
 
-        ////////////////////////////////////////////////////////////////////////////////////////////
-        eprintln!("Built model and tokenizer");
         let tokenizer = tokenizer
             .with_padding(None)
             .with_truncation(None)
@@ -113,29 +104,30 @@ impl Args {
         let tokens = tokenizer.encode(&*self.prompt, true)
             .map_err(|err| LuaError::external(err))?
             .get_ids().to_vec();
-        eprintln!("Got tokens");
         let token_ids = Tensor::new(&tokens[..], &self.device)
             .map_err(|err| {
-                eprintln!("token_ids ... Tensor::new\n {}", err);
+                // If there's a problem here, it is just going to panic and won't print this error.
+                // Issue with i64 and thread initialization in latest candle
+                eprintln!("!! Error on Tensor::new\n {}", err);
                 LuaError::external(err)
             })?
             .unsqueeze(0)
             .map_err(|err| {
-                eprintln!("token_ids ... unsqueeze\n {}", err);
+                eprintln!("!! Error on token_ids ... unsqueeze\n {}", err);
                 LuaError::external(err)
             })?;
         let token_type_ids = token_ids.zeros_like()
             .map_err(|err| {
-                eprintln!("token_type_ids ... zeros_like\n {}", err);
+                eprintln!("!! Error on token_type_ids\n {}", err);
                 LuaError::external(err)
             })?;
         let embeddings = model.forward(&token_ids, &token_type_ids)
             .map_err(|err| {
-                eprintln!("embeddings ... model.forward\n {}", err);
+                eprintln!("!! Error on model.forward\n {}", err);
                 LuaError::external(err)
             })?;
 
-        // Apply some avg-pooling by taking the mean embedding value for all tokens (including padding)
+        // Do average pooling - mean embedding value for all tokens w/ padding
         let (_n_sentence, n_tokens, _hidden_size) = embeddings.dims3()
             .map_err(|err| LuaError::external(err))?;
         let emb_sum = embeddings.sum(1)
@@ -150,78 +142,47 @@ impl Args {
         let embeddings_data = embeddings
             .flatten_all()
             .map_err(|err| {
-                eprintln!("!! Error during embeddings_data.flatten_all() ...\n{}", err);
+                eprintln!("!! Error on embeddings_data.flatten_all()\n{}", err);
                 LuaError::external(err)
             })?
             .to_vec1()
             .map_err(|err| {
-                eprintln!("!! Error during embeddings_data.to_vec1 ...\n{}", err);
+                eprintln!("!! Error on embeddings_data.to_vec1\n{}", err);
                 LuaError::external(err)
             })?;
-        // let shp = embeddings_data.shape()
-        //     .map_err(|err| LuaError::external(err))?;
 
         Ok(embeddings_data)
     }
 }
+// TODO: cleanup leftover thread stuff if not going to bring back global state or thread management
 // fn initialize_rayon() {
 //     ThreadPoolBuilder::new().num_threads(1).build_global().unwrap();
 // }
-// fn encode_text(_lua: &Lua, table: LuaTable) -> LuaResult<Vec<f32>> {
 fn encode_text(_lua: &Lua, table: LuaTable) -> LuaResult<String> {
-    eprintln!("Starting encode_text");
-    // TODO: remove the hard-coded model tokenizer config and get it from the passed table
+    // TODO: remove the hard-coded model tokenizer config and get it from the passed Lua table
     // let model: Option<String> = table.get("model")?;
     let model_id = "sentence-transformers/all-MiniLM-L6-v2";
-    let model = Some(include_str!("data/sentence-transformers_all-MiniLM-L6-v2/model.safetensors.b64").to_string());
-    eprintln!("Got model");
-    let model = match model {
-        Some(model) => b64.decode(model).map_err(|e| LuaError::external(e))?,
-        None => unreachable!(),
-    };
-    eprintln!("Decoded model");
-    // let config: Option<String> = table.get("config")?;
-    // let config = Some(include_str!("data/sentence-transformers_all-MiniLM-L6-v2/config.json.b64").to_string());
-    // let config = match config {
-    //     Some(config) => {
-    //         let dec = b64.decode(config)
-    //                 .map_err(|e| LuaError::external(e))?;
-    //         String::from_utf8(dec).map_err(|e| LuaError::external(e))?
-    //     },
-    //     None => unreachable!(),
-    // };
-    let config = include_str!("data/sentence-transformers_all-MiniLM-L6-v2/config.json.b64").to_string();
+    let model_b64 = include_str!("data/sentence-transformers_all-MiniLM-L6-v2/model.safetensors.b64").to_string();
+    let model = b64.decode(model).map_err(|e| LuaError::external(e))?;
 
-    eprintln!("Got config");
-    // let tokenizer: String = table.get("tokenizer")?;
-    let tokenizer = include_str!("data/sentence-transformers_all-MiniLM-L6-v2/tokenizer.json.b64");
-    let tokenizer = b64.decode(tokenizer)
-        .map_err(|e| LuaError::external(e))?;
-    eprintln!("Got tokenizer");
-
-    // print("Bert args parsed");
+    let config_b64 = include_str!("data/sentence-transformers_all-MiniLM-L6-v2/config.json.b64").to_string();
+    let tokenizer_b64 = include_str!("data/sentence-transformers_all-MiniLM-L6-v2/tokenizer.json.b64");
+    let tokenizer = b64.decode(tokenizer).map_err(|e| LuaError::external(e))?;
+    
     let args = Args {
         model, config, tokenizer,
-        prompt: table.get("prompt").unwrap_or("The cat sits outside".to_string()), // When set, compute embeddings for this prompt.
-        // use_safetensors: !table.get("use_safetensors").unwrap_or(true), // Use the pytorch weights rather than the safetensors ones
+        prompt: table.get("prompt").unwrap_or("I forgot to set a prompt somehow??".to_string()),
         normalize_embeddings: table.get("normalize_embeddings").unwrap_or(true), // L2 normalization for embeddings. default_value = "true"
         approximate_gelu: table.get("approximate_gelu").unwrap_or(false), // Use tanh based approximation for Gelu instead of erf implementation. default_value = "false"
         device: Device::Cpu,
     };
-    eprintln!("Prepped Args");
-    eprintln!("Prompt provided is: {}", args.prompt);
-
-    // let embeddings = _encode_text(args).map_err(|err| LuaError::external(err))?;
+    println!("Prompt provided is: {}", args.prompt);
+    
     let embeddings = args._encode_text()
         .map_err(|err| {
-            eprintln!("Error in encode_text when called _encode_text()\n{}", err);
+            eprintln!("Error in encode_text when calling Arg's _encode_text()\n{}", err);
             LuaError::external(err)
         })?;
-    // let embeddings: String = serde_json::to_string(&embeddings).map_err(|err| LuaError::external(err))?;
-    // println!("{}", embeddings);
-    // let embeddings = Embeddings {
-    //     data: embeddings,
-    // };
     let output = Embedding {
         data: embeddings,
         prompt: args.prompt,
@@ -232,7 +193,7 @@ fn encode_text(_lua: &Lua, table: LuaTable) -> LuaResult<String> {
             eprintln!("Error in serializing embeddings\n{}", err);
             LuaError::external(err)
         })?;
-    eprintln!("{}", output_str);
+    // println!("{}", output_str);
     Ok(output_str)
 }
 
